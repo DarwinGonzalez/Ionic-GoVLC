cdd6398a6c0e7c73ae843380223f6b8fafb0294f
DarwinGonzalez <alu0100812794@ull.edu.es>
2019-03-12 10:22:10 +0100

    Creating service to get monuments from JSON

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page implements OnInit {
+
+  public monuments = [];
+  constructor(private _apiService: ApiService) {}
+
+  ngOnInit() {
+    this._apiService.getMonumentJSON().subscribe( data => {
+      console.log(data);
+      data.forEach(element => {
+        this.monuments.push(element);
+      });
+      console.log(this.monuments);
+    });
+  }
+}
