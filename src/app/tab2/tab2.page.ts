8d1a036b9a6a774ea7f21589f8ad5ffe4e2a58ee
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-03-12 17:32:03 +0100

    Integrate google maps api in the project

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  @ViewChild("map") mapElement;
+  map: any;
+  constructor(){}
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  initMap() {
+    let coords = new google.maps.LatLng(39.4697495, -0.37739);
+    let mapOptions: google.maps.MapOptions = {
+      center: coords,
+      zoom: 12,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+}
